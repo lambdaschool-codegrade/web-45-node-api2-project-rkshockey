100a56a801ace615744b5557b7d76f34e9575534
rkshockey <rkshockey@gmail.com>
2021-08-31 15:49:25 -0400

    Installed and implemented cors

--- api/server.js
+++ api/server.js
@@ -1,11 +1,13 @@
 // implement your server here
 // require your posts router and connect it here
 const express = require('express')
+const cors = require('cors')
 const postsRouter = require('./posts/posts-router')
 
 const server = express()
 
 server.use(express.json())
+server.use(cors())
 
 server.use('/api/posts', postsRouter)
 
