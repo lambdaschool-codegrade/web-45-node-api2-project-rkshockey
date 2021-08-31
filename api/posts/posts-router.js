d3ec2b3281585f6cf0e1dfdc140b9792696e6e31
rkshockey <rkshockey@gmail.com>
2021-08-31 15:35:34 -0400

    Added get comments route

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -4,6 +4,28 @@ const Posts = require ('./posts-model')
 
 const router = express.Router()
 
+router.get('/:id/comments', (req, res) => {
+    Posts.findById(req.params.id)
+        .then(post => {
+            if (post !== undefined){
+                Posts.findPostComments(post.id)
+                    .then(comments => {
+                        res.status(200).json(comments)
+                    })
+                    .catch(err => {
+                        console.log(err)
+                        res.status(500).json({ message: "The comments information could not be retrieved" })
+                    })
+            }else{
+                res.status(404).json({ message: "The post with the specified ID does not exist" })
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ message: "The comments information could not be retrieved" })
+        })
+})
+
 router.get('/:id', (req, res) => {
     Posts.findById(req.params.id)
         .then(post => {
