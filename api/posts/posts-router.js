78a86ed70b6a6aa70c86105433b78ef1db837abf
rkshockey <rkshockey@gmail.com>
2021-08-31 14:04:14 -0400

    Added get by id

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,22 @@
 // implement your posts router here
+const express = require('express')
+const Posts = require ('./posts-model')
+
+const router = express.Router()
+
+router.get('/:id', (req, res) => {
+    Posts.findById(req.params.id)
+        .then(post => {
+            if (post){
+                res.status(200).json(post)
+            }else{
+                res.status(404).json({ message: "The post with the specified ID does not exist" })
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ message: "The post information could not be retrieved" })
+        })
+})
+
+module.exports = router
